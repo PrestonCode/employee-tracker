e6f93b082515705b5c2a32a77503c3fd968c58c9
Preston Patterson <prestonjpatterson@gmail.com>
2020-07-30 01:09:14 -0600

    a

--- 02-Homework/employee-table.sql
+++ 02-Homework/employee-table.sql
@@ -3,6 +3,23 @@ CREATE DATABASE employees;
 
 USE employees;
 
-CREATE TABLE employees (
-	id INTEGER AUTO_INCREMENT NOT NULL
+CREATE TABLE department (
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR (30) NOT NULL
 );
+
+CREATE TABLE role (
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL(8,2),
+    department_id INT
+);
+
+CREATE TABLE employees (
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
+
--- 02-Homework/surver.js
+++ 02-Homework/surver.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql")
+const inquirer = require("inquirer")
+
 const connection = mysql.createConnection({
     host: "localhost",
 
@@ -15,3 +17,93 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId + "\n");
     postItem();
 });
+
+const addItem = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "start",
+                message: "what are you adding?",
+                choices: ["Department", "Role", "Employee"]
+            }
+        ])
+        .then(answers => {
+            switch (answers) {
+                case "Department":
+                    inquirer
+                        .prompt([
+                            {
+                                type: "input",
+                                name: "departmentName",
+                                message: "what is the name of the department?"
+                            }
+                        ]).then
+                    break;
+                case "Role":
+                    inquirer
+                        .prompt([
+                            {
+                                type: "input",
+                                name: "roleTitle",
+                                message: "what is the title of the role?"
+                            }
+                        ])
+                    break;
+                default:
+                    inquirer
+                        .prompt([
+                            {
+                                type: "list",
+                                name: "addRole",
+                                message: "what role do you want to add?",
+                                choices: ["Department", "Role", "Employee"]
+                            },
+                            {
+                                type: "input",
+                                name: "whatFName",
+                                message: "what is there first name?",
+                            },
+                            {
+                                type: "input",
+                                name: "addLName",
+                                message: "what is there last name?",
+                            }
+                        ])
+            }
+            break;
+        }
+        }
+
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "start",
+            message: "what do you what to do?",
+            choices: ["View", "Add", "Edit", "Exit"]
+        }
+    ])
+    .then(answers => {
+        switch (answers) {
+            case "View":
+
+                break;
+            case "Add":
+
+                break;
+            case "Edit":
+                inquirer
+                    .prompt([
+                        {
+
+                        }
+                    ])
+                break;
+
+            default:
+
+                break;
+        }
+    })
